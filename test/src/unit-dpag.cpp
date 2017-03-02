f66dccc970cc3d139f5f6c4ec593a281560f5372
Alessandro Vullo <avullo@ebi.ac.uk>
2017-03-02 19:21:12 +0000

    Tests cases for different structures. Begin with sequence.

--- test/src/unit-dpag.cpp
+++ test/src/unit-dpag.cpp
@@ -1,8 +1,9 @@
 #include "catch.hpp"
 
 #include "DPAG.h"
+#include <cstdio>
 #include <vector>
-#include <iostream>
+#include <fstream>
 using namespace std;
 
 typedef unsigned int uint;
@@ -20,6 +21,7 @@ TEST_CASE("Basic DPAG usage", "[dpag]") {
   outIter out_i, out_end, out_i1;
   EdgeId edge_id = boost::get(boost::edge_index, d);
 
+  // access vertices with iterators and indices
   SECTION("accessing vertices") {
     boost::tie(v_i, v_end) = boost::vertices(d);
     SECTION("first vertex") {
@@ -54,21 +56,96 @@ TEST_CASE("Basic DPAG usage", "[dpag]") {
   }
 }
 
-// class DPAGGenerator {
-//   DPAG* dpags;
+class DPAGGenerator {
+  DPAG** dpags;
   
-// public:
-//   DPAGGenerator() {
-//     make_sequence("../data/sequence.gph");
-//     make_linear_chain("../data/sequence.gph");
-//   }
-  
-//   void make_sequence(const char* fname) {
-//   }
+public:
+
+  // build a sequence of T steps
+  DPAG make_sequence(uint T) {
+    DPAG s(T);
+
+    // make the connections go from right to left
+    // to emulate RNN unfolding from right to left,
+    // i.e. reverse topological sort
+    for(uint i=T-1; i>0; --i)
+      boost::add_edge(i, i-1, s);
     
-//   void make_linear_chain(const char* fname) {
-//   }
-// };
+    return s;
+  }
   
-// TEST_CASE("Different data structures", "[dpag]") {
-// }
+  DPAG make_dpag(const char* fname) {
+    // ifstream is(fname);
+    // if(!is) {
+    //   fprintf(stderr, "Could not open file %s\n", fname);
+    //   exit(1);
+    // }
+
+    DPAG d;
+
+    return d;
+  }
+
+  
+  DPAG make_grid(uint N) {
+    DPAG grid(N);
+
+    return grid;
+  }
+};
+  
+TEST_CASE("Various data structures", "[dpag]") {
+  //Vertex_d v;
+  vertexIt v_i, v_end;
+  outIter out_i, out_end;
+  ieIter in_i, in_end;
+
+  DPAGGenerator dg;
+
+  int T = 10;
+  DPAG sequence = dg.make_sequence(T);
+  
+  CHECK(boost::num_vertices(sequence) == T);
+
+  DPAG dpag = dg.make_dpag("../data/test_dpag.gph");
+  
+  int N = T;
+  DPAG grid = dg.make_grid(N);
+  CHECK(boost::num_vertices(grid) == N);
+
+  SECTION("data structures are different") {
+    CHECK_FALSE(equal(sequence, dpag));
+    CHECK_FALSE(equal(sequence, grid));
+    CHECK_FALSE(equal(dpag, grid));
+  }
+
+  SECTION("sequence connectivity") {
+    CHECK(max_indegree(sequence) == 1);
+    CHECK(max_outdegree(sequence) == 1);
+    
+    VertexId vertex_id = boost::get(boost::vertex_index, sequence);
+    EdgeId edge_id = boost::get(boost::edge_index, sequence);
+    
+    for(boost::tie(v_i, v_end) = boost::vertices(sequence); v_i!=v_end; ++v_i) {
+      boost::tie(out_i, out_end)=boost::out_edges(*v_i, sequence);
+      boost::tie(in_i, in_end)=boost::in_edges(*v_i, sequence);
+      
+      int id = vertex_id[*v_i];
+      if(id == 0) {
+	CHECK(boost::source(*in_i, sequence) == vertex_id[*(v_i+1)]);
+	CHECK(++in_i == in_end);
+	CHECK(out_i == out_end);
+      } else if(id < T-1) {
+	CHECK(boost::target(*out_i, sequence) == vertex_id[*(v_i-1)]);
+	CHECK(++out_i == out_end);
+	CHECK(boost::source(*in_i, sequence) == vertex_id[*(v_i+1)]);
+	CHECK(++in_i == in_end);
+      } else {
+	CHECK(id == T-1);
+	CHECK(boost::target(*out_i, sequence) == vertex_id[*(v_i-1)]);
+	CHECK(++out_i == out_end);
+	CHECK(in_i == in_end);
+      }
+    }
+  }
+}
