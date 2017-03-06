8dc5e1cc5078f22d6e4694fa4ad92dcd0070b6f1
Alessandro Vullo <avullo@ebi.ac.uk>
2017-03-06 20:14:14 +0000

    Initial support for factory method.

--- Instance.cpp
+++ Instance.cpp
@@ -1,15 +1,25 @@
-#include "StructuredDomain.h"
+#include "Instance.h"
 #include <cassert>
 using namespace std;
 
-Skeleton::Skeleton(Domain domain): _norient(numOrientations(domain)) {
-  assert(_norient > 0);
-  
-  _orientations = new DPAG[_norient];
-  _top_orders = new (vector<int>)[_orient];
+// factory method
+Instance* Instance::factory(Enum d, Transduction t, std::istream& is)
+  throw(BadInstanceCreation) {
+  if(type == SEQUENCE) { return new Sequence(d, t, is); }
 }
 
-Skeleton::~Skeleton() {
+Instance::Skeleton::Skeleton(Domain domain): _i(-1), _o(-1), _norient(num_orientations(domain)) {
+  assert(_norient > 0);
+  
+  _orientations = new (DPAG*)[_norient];
+  _top_orders = new (vector<int>)[_norient];
+}
+
+Instance::Skeleton::~Skeleton() {
+  for(int i=0; i<_norient; ++i)
+    delete _orientations[i];
   delete[] _orientations;
+  
   delete[] _top_orders;
 }
+
