74956f81f058a616c24056d1fbb1bcf6e454ae80
Alessandro Vullo <avullo@ebi.ac.uk>
2017-04-30 14:05:29 +0100

    Add convenient macros.

--- General.h
+++ General.h
@@ -1,6 +1,9 @@
-#ifndef _GENERAL_H_
-#define _GENERAL_H_
+#ifndef GENERAL_H
+#define GENERAL_H
+
+#define PR(x) cout << #x << ": " << x << endl
+#define WFP int z; cin >> z
 
 typedef unsigned int uint;
 
-#endif // _GENERAL_H_
+#endif // GENERAL_H
