fc58e87f9959b08c50ad41ebc41d0a8ce414c235
Alessandro Vullo <avullo@ebi.ac.uk>
2018-04-09 14:48:30 +0100

    Utils to generate random numbers suitable to initialise network weights.

--- General.h
+++ General.h
@@ -1,9 +1,20 @@
 #ifndef GENERAL_H
 #define GENERAL_H
 
+#define BIGRND 0x7fffffff
 #define PR(x) cout << #x << ": " << x << endl
 #define WFP int z; cin >> z
 
 typedef unsigned int uint;
+
+// Generate a random number between 0.0 and 1.0
+double rnd01() {
+  return ((double) random() / (double) BIGRND);
+}
+
+// Generate a random number between -1.0 and +1.0
+double nrnd01() {
+  return ((rnd01() * 2.0) - 1.0);
+}
 
 #endif // GENERAL_H
