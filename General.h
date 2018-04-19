e6147b29c71391bf304fecfa6f9b647b56258984
Alessandro Vullo <avullo@ebi.ac.uk>
2018-04-19 09:32:25 +0100

    These are not general enough at the moment, moved to the RNN implementation.

--- General.h
+++ General.h
@@ -7,14 +7,4 @@
 
 typedef unsigned int uint;
 
-// Generate a random number between 0.0 and 1.0
-double rnd01() {
-  return ((double) random() / (double) BIGRND);
-}
-
-// Generate a random number between -1.0 and +1.0
-double nrnd01() {
-  return ((rnd01() * 2.0) - 1.0);
-}
-
 #endif // GENERAL_H
